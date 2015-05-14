e4d01f64fdcefe79fcabfef7ce6d7e0ec804f87b
sampable@mac <sampable@gmail.com>
2015-05-14 22:00:56 +0800

    define the skeleton of heap module

---
+++ heap.py
@@ -0,0 +1,42 @@
+def min(minheap):
+	pass
+
+def min_push(minheap, item):
+	pass
+
+def min_pop(minheap):
+	pass
+
+# pop then push
+def min_replace(minheap, item):
+	pass
+
+# push then pop
+def min_pushpop(minheap, item):
+	pass
+
+def min_heapify(x):
+	pass
+
+def min_nlargest(minheap, n):
+	pass
+
+def min_nsmallest(minheap, n):
+	pass
+
+def min_create(x):
+	pass
+
+# advance
+def min_merge(minheap1, minheap2):
+	pass
+
+# advance
+def min_meld(minheap1, minheap2):
+	pass
+
+def __min_decreasekey(minheap, idx, key):
+	pass
+
+def __min_delete(minheap, idx):
+	pass

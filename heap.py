96dc692718a76e6a7390b54c09404caa9836729b
sampable@mac <sampable@gmail.com>
2015-05-16 11:57:04 +0800

    implement basic functions of min heap operations and their tests

--- heap.py
+++ heap.py
@@ -1,43 +1,78 @@
 def min(minheap):
-	pass
+	return minheap[0]
 
 def min_push(minheap, item):
-	pass
+	minheap.append(item)
+	__min_shiftup(minheap, len(minheap) - 1)
 
 def min_pop(minheap):
-	pass
+	minval = minheap[0]
+	minheap[0] = minheap[len(minheap) - 1]
+	del minheap[len(minheap) - 1]
+	__min_shiftdown(minheap, 0)
+	return minval
 
-# pop then push
+# pop then push, return minimum before push
 def min_replace(minheap, item):
-	pass
+	minval = minheap[0]
+	minheap[0] = item
+	__min_shiftdown(minheap, 0)
+	return minval
 
-# push then pop
+# push then pop, return minimum including new item
 def min_pushpop(minheap, item):
-	pass
+	if item <= minheap[0]:
+		return item
+	return min_replace(minheap, item)
 
 def min_heapify(x):
-	pass
+	# (len(x) - 1) // 2 is the last parent node of heap
+	for i in range((len(x) - 1) // 2, -1, -1):
+		__min_shiftdown(x, i)
 
 def min_nlargest(minheap, n):
-	pass
+	copy = minheap[:]
+	result = []
+	for i in range(len(minheap)):
+		result.append(min_pop(copy))
+	return list(reversed(result))[:n]
 
 def min_nsmallest(minheap, n):
-	pass
-
-# build min-heap from array
-def min_build(x):
-	pass
+	copy = minheap[:]
+	result = []
+	for i in range(n):
+		result.append(min_pop(copy))
+	return result
 
 # advance
 def min_merge(minheap1, minheap2):
-	pass
+	longcopy, shortcopy = minheap1[:], minheap2[:]
+	if len(longcopy) < len(shortcopy):
+		longcopy, shortcopy = shortcopy, longcopy
+	for i in range(len(shortcopy)):
+		min_push(longcopy, min_pop(shortcopy))
+	return longcopy
 
 # advance
 def min_meld(minheap1, minheap2):
-	pass
+	for i in range(len(minheap2)):
+		min_push(minheap1, min_pop(minheap2))
+	return minheap1
 
-def __min_decreasekey(minheap, idx, key):
-	pass
+def __min_shiftdown(x, idx):
+	min_idx = idx
+	if idx * 2 + 1 < len(x) and x[idx * 2 + 1] < x[idx]:
+		min_idx = idx * 2 + 1
+	if idx * 2 + 2 < len(x) and x[idx * 2 + 2] < x[min_idx]:
+		min_idx = idx * 2 + 2
+	if min_idx != idx:
+		x[idx], x[min_idx] = x[min_idx], x[idx]
+		__min_shiftdown(x, min_idx)
 
-def __min_delete(minheap, idx):
-	pass
+def __min_shiftup(x, idx):
+	if idx <= 0:
+		return
+	if x[(idx - 1) // 2] > x[idx]:
+		x[idx], x[(idx - 1) // 2] = x[(idx - 1) // 2], x[idx]
+		__min_shiftup(x, (idx - 1) // 2)
+
---
+++ test/test_heap.py
@@ -0,0 +1,150 @@
+import unittest
+from .. import heap
+
+class test_heap(unittest.TestCase):
+	def test_min_heapify(self):
+		a = [10, 9]
+		heap.min_heapify(a)
+		self.assertEqual(a[0], 9)
+		self.assertEqual(a[1], 10)
+		
+		b = [30, 29, 20, 19, 10, 9]
+		heap.min_heapify(b)
+		self.assertTrue(b[0] < b[1])
+		self.assertTrue(b[0] < b[2])
+		self.assertTrue(b[1] < b[3])
+		self.assertTrue(b[1] < b[4])
+		self.assertTrue(b[2] < b[5])
+
+	def test_min_pop(self):
+		a = [100, 99, 89, 90, 90, 100, 2, 54]
+		heap.min_heapify(a)
+		prev = heap.min_pop(a)
+		for i in range(len(a) - 1):
+			cur = heap.min_pop(a)
+			self.assertTrue(cur >= prev)
+			prev = cur
+
+		b = [5, 5, 4, -10]
+		heap.min_heapify(b)
+		self.assertEqual(heap.min_pop(b), -10)
+		self.assertEqual(heap.min_pop(b), 4)
+		self.assertEqual(heap.min_pop(b), 5)
+		self.assertEqual(heap.min_pop(b), 5)
+
+		c = [1]
+		heap.min_heapify(c)
+		self.assertEqual(heap.min_pop(c), 1)
+		self.assertEqual(c, [])
+
+		e = []
+		heap.min_heapify(e)
+		with self.assertRaises(IndexError):
+			heap.min_pop(e)
+
+	def test_min(self):
+		a = [10, 5, 1]
+		heap.min_heapify(a)
+		self.assertEqual(heap.min(a), 1)
+
+		b = [3, 2, 7, 10, 5, 2]
+		heap.min_heapify(b)
+		self.assertEqual(heap.min(b), 2)
+
+		e = []
+		heap.min_heapify(e)
+		with self.assertRaises(IndexError):
+			heap.min(e)
+
+	def test_min_push(self):
+		a = []
+		heap.min_push(a, 5)
+		self.assertEqual(a[0], 5)
+		heap.min_push(a, 7)
+		self.assertEqual(a[0], 5)
+		heap.min_push(a, 3)
+		self.assertEqual(a[0], 3)
+		heap.min_push(a, 3)
+		self.assertEqual(a[0], 3)
+		self.assertEqual(len(a), 4)
+
+		b = [100, 7, 14, 25]
+		heap.min_heapify(b)
+		heap.min_push(b, 35)
+		self.assertEqual(b[0], 7)
+		heap.min_push(b, 3)
+		self.assertEqual(b[0], 3)
+		heap.min_push(b, -2)
+		self.assertEqual(b[0], -2)
+		heap.min_push(b, -5)
+		self.assertEqual(b[0], -5)
+
+	def test_min_nsmallest(self):
+		a = [10, 20, 25, 5, -1, 100]
+		heap.min_heapify(a)
+		self.assertEqual(heap.min_nsmallest(a, 0), [])
+		self.assertEqual(heap.min_nsmallest(a, 1), [-1])
+		self.assertEqual(heap.min_nsmallest(a, 2), [-1, 5])
+		self.assertEqual(heap.min_nsmallest(a, 3), [-1, 5, 10])
+		self.assertEqual(heap.min_nsmallest(a, 4), [-1, 5, 10, 20])
+		self.assertEqual(heap.min_nsmallest(a, 5), [-1, 5, 10, 20, 25])
+		self.assertEqual(heap.min_nsmallest(a, 6), [-1, 5, 10, 20, 25, 100])
+		with self.assertRaises(IndexError):
+			heap.min_nsmallest(a, 7)
+
+	def test_min_nlargest(self):
+		a = [10, 20, 25, 5, -1, 100]
+		heap.min_heapify(a)
+		self.assertEqual(heap.min_nlargest(a, 0), [])
+		self.assertEqual(heap.min_nlargest(a, 1), [100])
+		self.assertEqual(heap.min_nlargest(a, 2), [100, 25])
+		self.assertEqual(heap.min_nlargest(a, 3), [100, 25, 20])
+		self.assertEqual(heap.min_nlargest(a, 4), [100, 25, 20, 10])
+		self.assertEqual(heap.min_nlargest(a, 5), [100, 25, 20, 10, 5])
+		self.assertEqual(heap.min_nlargest(a, 6), [100, 25, 20, 10, 5, -1])
+		self.assertEqual(heap.min_nlargest(a, 7), [100, 25, 20, 10, 5, -1])
+
+	def test_min_replace(self):
+		a = [10, 20, 25, 5, -1, 100]
+		heap.min_heapify(a)
+		self.assertEqual(heap.min_replace(a, -4), -1)
+		self.assertEqual(a[0], -4)
+		self.assertEqual(heap.min_replace(a, 7), -4)
+		self.assertEqual(a[0], 5)
+
+	def test_min_pushpop(self):
+		a = [10, 20, 25, 5, -1, 100]
+		heap.min_heapify(a)
+		self.assertEqual(heap.min_pushpop(a, -4), -4)
+		self.assertEqual(a[0], -1)
+		self.assertEqual(heap.min_pushpop(a, 7), -1)
+		self.assertEqual(a[0], 5)
+
+	def test_min_merge(self):
+		a = [5, 3, 1]
+		b = [6, 4, 2, 1]
+		heap.min_heapify(a)
+		heap.min_heapify(b)
+		c = heap.min_merge(a, b)
+		prev = heap.min_pop(c)
+		for i in range(len(c)):
+			cur = heap.min_pop(c)
+			self.assertTrue(cur >= prev)
+			prev = cur
+
+	def test_min_meld(self):
+		a = [5, 3, 1]
+		b = [6, 4, 2, 1]
+		heap.min_heapify(a)
+		heap.min_heapify(b)
+		heap.min_meld(a, b)
+		self.assertEqual(len(a), 7)
+		self.assertEqual(len(b), 0)
+		prev = heap.min_pop(a)
+		for i in range(len(a)):
+			cur = heap.min_pop(a)
+			self.assertTrue(cur >= prev)
+			prev = cur
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
